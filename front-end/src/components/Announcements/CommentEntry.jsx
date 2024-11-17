a1368a0a436f1621c6de800d69a7f6b969a07339
Loay Al-Said <l.programming555@gmail.com>
2024-11-17 16:59:40 +0200

    Connect CommentEntry to the delete thunk

--- front-end/src/components/Announcements/CommentEntry.jsx
+++ front-end/src/components/Announcements/CommentEntry.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Dot, Trash2 } from 'lucide-react';
 import {
   selectUserRole,
   selectUserId
 } from '../../redux/selectors/uiSelectors';
 import { formatDate } from '../../utils/utilFunctions';
+import { deleteAnnouncementComment } from '../../redux/actions/announcementsThunks';
 
 export default function CommentEntry({ content }) {
   const userRole = useSelector(selectUserRole);
   const userId = useSelector(selectUserId);
   const date = formatDate(content.get('updatedAt'));
+  const dispatch = useDispatch();
 
   const handleDeleteComment = () => {
     if (window.confirm(`Are you sure you are deleting this comment ${content.get('id')}`)) {
-      console.log(content.get('id'));
+      const announcementId = content.get('announcementId');
+      const id = content.get('id');
+
+      dispatch(
+        deleteAnnouncementComment(announcementId, id)
+      )
     }
   }
 
