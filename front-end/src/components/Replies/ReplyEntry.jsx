c465f026d8b2c9c03033235d182bfb8d5c3559bb
Loay Al-Said <l.programming555@gmail.com>
2024-11-19 17:02:19 +0200

    Just some styling issues

--- front-end/src/components/Replies/ReplyEntry.jsx
+++ front-end/src/components/Replies/ReplyEntry.jsx
@@ -9,9 +9,12 @@ import {
 } from '../../redux/selectors/DiscussionsSelectors';
 import {
   selectUserRole,
-  selectUserId
+  selectUserId,
 } from '../../redux/selectors/uiSelectors';
-import { toggleReplyVote, deleteReply } from '../../redux/actions/discussionsThunks';
+import {
+  toggleReplyVote,
+  deleteReply,
+} from '../../redux/actions/discussionsThunks';
 
 export default function ReplyEntry({ content, questionId }) {
   const userRole = useSelector(selectUserRole);
@@ -27,12 +30,14 @@ export default function ReplyEntry({ content, questionId }) {
   );
   const date = formatDate(content.get('updatedAt'));
   const handleDeleteReply = () => {
-    if (window.confirm(`Are you sure you are deleting reply ${content.get('id')}`)) {
+    if (
+      window.confirm(`Are you sure you are deleting reply ${content.get('id')}`)
+    ) {
       dispatch(deleteReply(questionId, content.get('id')));
     }
 
     setShowOptions(false);
-  }
+  };
 
   function toggleVote() {
     dispatch(toggleReplyVote(content.get('id'), questionId));
@@ -40,16 +45,21 @@ export default function ReplyEntry({ content, questionId }) {
 
   return (
     <div className="card mb-2 p-3 d-flex flex-row align-items-start">
-      <img 
-        src={content?.getIn(['user', 'pictureThumbnail']) || 'https://picsum.photos/50'} 
-        alt="Replier" 
+      <img
+        src={
+          content?.getIn(['user', 'pictureThumbnail']) ||
+          'https://picsum.photos/50'
+        }
+        alt="Replier"
         className="rounded-circle me-3"
         width="50"
         height="50"
       />
       <div className="flex-grow-1">
         <h6>{content?.getIn(['user', 'name']) || 'Anonymous'}</h6>
-        <p className="text-muted">{content ? formatDate(content.get('updatedAt')) : ''}</p>
+        <p className="text-muted">
+          {content ? formatDate(content.get('updatedAt')) : ''}
+        </p>
         <div dangerouslySetInnerHTML={{ __html: content?.get('body') || '' }} />
       </div>
       {/* <div>
@@ -59,8 +69,12 @@ export default function ReplyEntry({ content, questionId }) {
         <div dangerouslySetInnerHTML={{ __html: content.get('body') }} />
       </div> */}
       {/* side buttons */}
-      <div  className="d-flex align-items-center ms-3">
-      <button  className={`btn btn-outline-${upvoted ? 'dark' : 'secondary'}`} type="button" onClick={toggleVote}>
+      <div className="d-flex align-items-center ms-3">
+        <button
+          className={`btn btn-outline-${upvoted ? 'dark' : 'secondary'}`}
+          type="button"
+          onClick={toggleVote}
+        >
           {upvotes}
 
           {!upvoted ? (
@@ -70,30 +84,29 @@ export default function ReplyEntry({ content, questionId }) {
           )}
         </button>
         {/* Let the menue menu empty for now */}
-        <button type="button" className="btn btn-light mt-2"
+        <button
+          type="button"
+          className="btn btn-light mt-2"
           onClick={() => setShowOptions(!showOptions)}
-          >
+        >
           <EllipsisVertical />
         </button>
-        {
-          showOptions &&
-          <div>          
-          <ul>
-            {
-              (userRole !== 'student' || userId === content.getIn(['user', 'id'])) &&
-              <li>
-              <button type='button' onClick={handleDeleteReply} >
-                <Trash2 color='red' />
-                Delete reply
-              </button>
-            </li>
-            }
-          </ul>
-        </div>
-        }
+        {showOptions && (
+          <div>
+            <ul>
+              {(userRole !== 'student' ||
+                userId === content.getIn(['user', 'id'])) && (
+                <li>
+                  <button type="button" onClick={handleDeleteReply}>
+                    <Trash2 color="red" />
+                    Delete reply
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
-
-
   );
 }
