490392bd00670958385f00db2ff9be74a142a4be
Loay Al-Said <l.programming555@gmail.com>
2024-11-20 17:03:16 +0200

    Add lectureEdited selector

--- front-end/src/redux/selectors/lecturesSelectors.js
+++ front-end/src/redux/selectors/lecturesSelectors.js
@@ -5,3 +5,5 @@ export const selectcourseSectionsJS = createSelector(
   (state) => state.lectures.get('sections'),
   (sections) => sections.toJS()
 );
+
+export const selectLectureEditedFlag = (state) => state.lectures.get('lectureEdited');
