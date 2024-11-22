095844d2c6ed4866d7e1af7e8c6917a569bd76bb
Loay Al-Said <l.programming555@gmail.com>
2024-11-22 19:23:11 +0200

    Move Comunites into a separate component

---
+++ front-end/src/components/Sidebar/Communities.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+
+
+export default function Communities() {
+	return (
+		<details>
+                  <summary>
+                    <i className="fa fa-users"></i> Communities
+                  </summary>
+                  <ul className="fa-ul ms-3">
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://cs50.bsky.social"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Bluesky
+                      </a>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://www.clubhouse.com/club/cs50"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Clubhouse
+                      </a>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://discord.gg/cs50"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Discord
+                      </a>
+                      <span className="badge bg-light ms-1 py-1 rounded-pill text-dark">
+                        Q&amp;A
+                      </span>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://cs50.edx.org/ed"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Ed
+                      </a>
+                      <span className="badge bg-light ms-1 py-1 rounded-pill text-dark">
+                        Q&amp;A
+                      </span>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://www.facebook.com/groups/cs50/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Facebook Group
+                      </a>
+                      <span className="badge bg-light ms-1 py-1 rounded-pill text-dark">
+                        Q&amp;A
+                      </span>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://www.facebook.com/cs50/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Facebook Page
+                      </a>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://github.com/cs50"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        GitHub
+                      </a>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://gitter.im/cs50/x"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Gitter
+                      </a>
+                      <span className="badge bg-light ms-1 py-1 rounded-pill text-dark">
+                        Q&amp;A
+                      </span>
+                    </li>
+                    <li data-marker="*" className="small">
+                      <span className="fa-li">
+                        <i className="fas fa-square"></i>
+                      </span>
+                      <a
+                        href="https://instagram.com/cs50"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Instagram
+                      </a>
+                    </li>
+                  </ul>
+                </details>
+	)
+}
