6c480a91c09bd5a7c4c8a6daa3e9d1a7fb85a980
Loay Al-Said <l.programming555@gmail.com>
2024-11-22 23:03:45 +0200

    Squashed commit of the following:
    commit bdbecc18940115fba44e7a9c38da51e198471b29
    Author: Ogbonna George Ekwueme <georgekwm1@gmail.com>
    Date:   Fri Nov 22 21:50:03 2024 +0100
    
        "Updated CORS_ALLOWED_ORIGINS in settings.py and modified db.sqlite3"
    
    commit da44eb9a8c4265371371d2d627ef33c9e1695e11
    Merge: 9934a1c 967ae0f
    Author: Ogbonna George Ekwueme <georgekwm1@gmail.com>
    Date:   Fri Nov 22 21:38:31 2024 +0100
    
        Merge branch 'JWT-setup' of https://github.com/georgekwm1/School-Hub into JWT-setup
    
    commit 9934a1ce0240014e1983a5dfa782e32e9af2fd74
    Author: Ogbonna George Ekwueme <georgekwm1@gmail.com>
    Date:   Fri Nov 22 21:37:50 2024 +0100
    
        "Update DOMAIN constant in constants.js and modify db.sqlite3"

--- schoolhub/settings.py
+++ schoolhub/settings.py
@@ -211,6 +211,9 @@ APPEND_SLASH = False
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3001', 'http://127.0.0.1:3001']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://localhost:3000",
+    "http://localhost:3001",
+    "https://localhost:3001",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
