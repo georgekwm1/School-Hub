1771774d3d73db3219177297fd5bd2b112eaa1d5
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-11-22 22:24:13 +0100

    Remove "https://localhost:3001/School-Hub" from CSRF_TRUSTED_ORIGINS and CORS_ALLOWED_ORIGINS lists.

--- schoolhub/settings.py
+++ schoolhub/settings.py
@@ -216,7 +216,7 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost:3000",
                         "https://127.0.0.1:3000",
                         "http://127.0.0.1:3001",
                         "https://127.0.0.1:3001",
-                        "https://localhost:3001/School-Hub",
+
                         ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -227,7 +227,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "https://127.0.0.1:3001",
-    "https://localhost:3001/School-Hub",
+
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
