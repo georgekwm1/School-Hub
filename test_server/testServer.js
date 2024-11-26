fcffce4ddfa022ad004501bfa12b2f1309d90c6b
Loay Al-Said <l.programming555@gmail.com>
2024-11-26 22:58:11 +0200

    Setup the questionsRouter

--- test_server/testServer.js
+++ test_server/testServer.js
@@ -4,14 +4,11 @@ const fs = require('fs');
 const ImageKit = require('imagekit');
 const app = express();
 const cors = require('cors');
-const port = 3000;
 
 const authRouter = require('./routes/auth');
 const lecturesRouter = require('./routes/lectures');
+const questionsRouter = require('./routes/questions');
 
-
-const key = fs.readFileSync('./key.pem');
-const cert = fs.readFileSync('./cert.pem');
 
 app.use(express.json());
 app.use(cors({origin: '*'}));
@@ -24,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use('/auth', authRouter);
 app.use(lecturesRouter);
+app.use(questionsRouter);
 
 
 const {
@@ -37,93 +35,6 @@ const {
   repliesList,
 } = require('./mockData');
 
-app.get('/lectures/:id/discussion', (req, res) => {
-  console.log(33)
-  const id = req.params.id;
-  const allowedLectures = mockSections.flatMap(section => section.lectures.map(lecture => lecture.id));
-
-  if (allowedLectures.includes(id)) {
-      const discussionWithLectureId = mockDiscussion.map(discussion => ({
-        ...discussion,
-        lectureId: id,
-      }));
-      res.json(discussionWithLectureId);
-
-    } else {
-    res.status(404).send({ message: 'Lecture not found' });
-  }
-});
-
-app.post('/lectures/:id/discussion', (req, res) => {
-  const lectureId = req.params.id;
-  const { userId, title, body } = req.body;
-
-  if (!userId || !title || !body) {
-    return res.status(400).send({ message: 'Missing required fields' });
-  }
-
-  const newEntry = {
-    id: `entry-${Date.now()}`,
-    title,
-    user: {
-      id: 'testId',
-      name: 'John Doe',
-      pictureThumbnail: `https://picsum.photos/200/${Math.floor(Math.random() * 100) + 300}`,
-    },
-    updatedAt: new Date().toISOString(),
-    upvotes: 0,
-    upvoted: false,
-    repliesCount: 0,
-    body,
-    lectureId
-  };
-
-  // Here you would typically add the newEntry to your database or data store.
-  // For this example, we'll just return it in the response.
-  mockDiscussion.unshift(newEntry);
-
-
-  res.status(201).json(newEntry);
-});
-
-app.get('/courses/:id/general_discussion', (req, res) => {
-  const id = req.params.id;
-  if (id === "testId") {
-    res.json(mockDiscussion);
-  } else {
-    res.status(404).send({ message: 'Course not found' });
-  }
-});
-
-app.post('/courses/:id/general_discussion', (req, res) => {
-  const courseId = req.params.id;
-  const { userId, title, body } = req.body;
-
-  if (!userId || !title || !body) {
-    return res.status(400).send({ message: 'Missing required fields' });
-  }
-
-  const newEntry = {
-    id: `entry-${Date.now()}`,
-    title,
-    user: {
-      id: 'testId',
-      name: 'John Doe',
-      pictureThumbnail: `https://picsum.photos/200/${Math.floor(Math.random() * 100) + 300}`,
-    },
-    updatedAt: new Date().toISOString(),
-    upvotes: 0,
-    upvoted: false,
-    repliesCount: 0,
-    body
-  };
-
-  // Here you would typically add the newEntry to your database or data store.
-  // For this example, we'll just return it in the response.
-  mockDiscussion.push(newEntry);
-
-  res.status(201).json(newEntry);
-});
 
 app.get('/questions/:id/replies', (req, res) => {
   const questionId = req.params.id;
@@ -164,29 +75,6 @@ app.post('/questions/:id/replies', (req, res) => {
   mockReplies.unshift(newReply);
 
   res.status(201).json(newReply);
-});
-
-app.post('/questions/:id/vote', (req, res) => {
-  const questionId = req.params.id;
-  const { action } = req.body;
-
-  if (!action) {
-    return res.status(400).send({ message: 'Missing required fields' });
-  }
-
-  const index = mockDiscussion.findIndex((question) => question.id === questionId);
-
-  if (index === -1) {
-    return res.status(404).send({ message: 'Question not found' });
-  }
-
-  if (action === 'upvote') {
-    mockDiscussion[index].upvotes += 1;
-  } else if (action === 'downvote') {
-    mockDiscussion[index].upvotes -= 1;
-  }
-
-  res.status(200).json(mockDiscussion[index]);
 });
 
 app.get('/courses/:id/announcements', (req, res) => {
@@ -294,16 +182,7 @@ app.post('/announcements/:id/comments', (req, res) => {
   res.status(201).json(newComment);
 });
 
-app.delete('/questions/:id', (req, res) => {
-  const questionId = req.params.id;
-  const index = mockDiscussion.findIndex((question) => question.id === questionId);
 
-  if (index === -1) {
-    return res.status(404).send({ message: 'Question not found' });
-  }
-  mockDiscussion.splice(index, 1);
-  res.status(200).json({ message: 'Question deleted successfully' });
-});
 
 app.delete('/replies/:id', (req, res) => {
   const replyId = req.params.id;
@@ -372,21 +251,7 @@ app.put('/comments/:id', (req, res) => {
   res.status(200).json(mockComments[index]);
 });
 
-app.put('/questions/:id', (req, res) => {
-  const { id } = req.params;
-  const { title, body } = req.body;
 
-  const index = mockDiscussion.findIndex((question) => question.id === id);
-
-  if (index === -1) {
-    return res.status(404).send({ message: 'Question not found' });
-  }
-
-  mockDiscussion[index].title = title;
-  mockDiscussion[index].body = body;
-
-  res.status(200).json(mockDiscussion[index]);
-});
 
 app.put('/replies/:id', (req, res) => {
   const { id } = req.params;
@@ -408,6 +273,11 @@ app.use((req, res, next) => {
   res.status(404).send({ message: 'Not found' });
 });
 
+
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
+
+const port = 3000;
 const httpsServer = https.createServer({ key, cert }, app);
 httpsServer.listen(port, () => {
   console.log(`Server started on https://localhost:${port}`);
