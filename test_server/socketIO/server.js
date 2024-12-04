100b0da38f66872f4c0d1a2dca9474e3501baae7
Loay Al-Said <l.programming555@gmail.com>
2024-12-04 23:29:21 +0200

    Add the authMiddleware to the socketIO server

--- test_server/socketIO/server.js
+++ test_server/socketIO/server.js
@@ -1,4 +1,23 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (io) => {
+	function authMiddleware(socket, next) {
+		const token = socket.handshake.auth.token;
+		if (!token) {
+			next(new Error('No token provided'));
+		}
+
+		try {
+			const payload = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+			socket.userId = payload.userId;
+			next();
+		} catch (error) {
+			next(new Error('Invalid token'));
+		}
+	}
+
+	io.use(authMiddleware);
+
 	io.on('connection', (socket) => {
 		console.log(`New socker connected ${socket.id}`);
 	});
