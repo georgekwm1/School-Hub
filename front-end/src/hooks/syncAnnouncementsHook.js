92c0ccda8ff8b34e1dd950675bfc25b35125383b
Loay Al-Said <l.programming555@gmail.com>
2024-12-06 14:17:41 +0200

    Add announcementDeleted handler to the useSyncAnnouncements hook

--- front-end/src/hooks/syncAnnouncementsHook.js
+++ front-end/src/hooks/syncAnnouncementsHook.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSocket } from '../socket';
-import { addAnnouncementSuccess } from '../redux/actions/announcementsActionCreators';
+import { addAnnouncementSuccess, deleteAnnouncementSuccess } from '../redux/actions/announcementsActionCreators';
 import { selectIsSocketReady } from '../redux/selectors/uiSelectors';
 
 export default function useSyncAnnouncements () {
@@ -17,8 +17,22 @@ export default function useSyncAnnouncements () {
         dispatch(addAnnouncementSuccess(payload, lastFetched));
       });
       
+      // Sync deletion
+      socket.on('announcementDeleted', ({payload}) => {
+        const { announcementId } = payload;
+        // I'm abit sceptical here.. because this action
+        // sets isLoading to false and error to null
+        // This means that this action can  intercept another work flow.
+        // I think I have to see a better way.. may be even create a specific setup 
+        // For this or set a conditional in the reducer.. to metigate this!
+        // But diffentely not now.. no time whatsoever..
+        // With the next sprint
+        dispatch(deleteAnnouncementSuccess(announcementId));
+      })
+
       return () => {
         socket.off('announcementCreated');
+        socket.off('announcementDeleted');
       };      
     }
   }, [dispatch, getSocket, isSocketReady]);
