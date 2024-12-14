a5f2d091530f32f7f226096f32097b3ff1458ed1
Loay Al-Said <l.programming555@gmail.com>
2024-12-14 23:44:42 +0200

    Listen from commnetCCreated in sync comments hook

--- front-end/src/hooks/syncCommentsHook.js
+++ front-end/src/hooks/syncCommentsHook.js
@@ -1,17 +1,25 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {selectIsSocketReady} from '../redux/selectors/uiSelectors';
+import { selectIsSocketReady } from '../redux/selectors/uiSelectors';
 import { getSocket } from '../socket';
 import { addCommentSuccess } from '../redux/actions/announcementsActionCreators';
 
-
 export default function useSyncComments(announcementId) {
-	const dispatch = useDispatch();
-	const isSocketReady = useSelector(selectIsSocketReady);
-	const socket = getSocket();
+  const dispatch = useDispatch();
+  const isSocketReady = useSelector(selectIsSocketReady);
+  const socket = getSocket();
 
-	useEffect(() => {
-		if (socket) {
-		}
-	}, [dispatch, isSocketReady, socket]);
+  useEffect(() => {
+    if (socket) {
+      // Sync Creation
+      socket.on('commentCreated', ({ payload }) => {
+        const { announcementId, newComment } = payload;
+        dispatch(addCommentSuccess(announcementId, newComment));
+      });
+
+      return () => {
+        socket.off('commentCreated');
+      };
+    }
+  }, [dispatch, isSocketReady, socket]);
 }
