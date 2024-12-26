10952cd1212f3bf59751bf8de2d7f8182f2b1368
Loay Al-Said <l.programming555@gmail.com>
2024-12-26 14:41:20 +0200

    Create the db.js file and export the db interface
    Export an  object with props. [query, execute, transaction, pool and I forgot]

---
+++ test_server/db.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+})
+
+module.exports = {
+	query: async (query, params) => {
+		const [results] = await pool.query(query, params);
+		return results;
+	},
+
+	execute: async (query, params) => {
+		const [results] = await pool.execute(query, params);
+		return results;
+	},
+	pool,
+	transaction: async (callback) => {
+		const connection = await pool.getConnection();
+		try {
+			await connection.beginTransaction();
+			const result = await callback(connection);
+			await connection.commit();
+			return result;
+		} catch (error) {
+			await connection.rollback();
+			throw error;
+		} finally {
+			connection.release();
+		}
+	},
+}
