0560b672c1911a38430b6d52e84e8b1820662a23
Loay Al-Said <l.programming555@gmail.com>
2025-03-01 07:14:30 +0200

    Make the DB setup script add another user for remote connection

--- test_server/setup_mysql_dev.sql
+++ test_server/setup_mysql_dev.sql
@@ -1,4 +1,16 @@
-CREATE USER  IF NOT EXISTS 'pro_learning_hub_dev_user'@'localhost' IDENTIFIED  WITH mysql_native_password BY 'devPwd_1';
 CREATE DATABASE IF NOT EXISTS pro_learning_hub_dev_db;
-GRANT ALL PRIVILEGES ON pro_learning_hub_dev_db.* TO 'pro_learning_hub_dev_user'@'localhost';
+
+CREATE USER IF NOT EXISTS 'pro_learning_hub_dev_user'@'197.54.215.222'
+IDENTIFIED WITH mysql_native_password BY 'devPwd_1';
+
+GRANT ALL PRIVILEGES ON pro_learning_hub_dev_db.* 
+TO 'pro_learning_hub_dev_user'@'197.54.215.222';
+
+-- User for app running on the server
+CREATE USER IF NOT EXISTS 'pro_learning_hub_dev_user'@'localhost'
+IDENTIFIED WITH mysql_native_password BY 'devPwd_1';
+
+GRANT ALL PRIVILEGES ON pro_learning_hub_dev_db.* 
+TO 'pro_learning_hub_dev_user'@'localhost';
+
 FLUSH PRIVILEGES;
