d031eb5519dd9e20d575811bfa058f2b9ceabde8
Loay Al-Said <l.programming555@gmail.com>
2025-03-02 08:32:45 +0200

    Mv the Django fiels to a separete directory

---
+++ DjangoServer/schoolhub/passkeys.py
@@ -0,0 +1,11 @@
+SECRET_KEY = "django-insecure-u607#!i!*3-0df+xdq=v5+r!-aw^h%l!x^1ogcio%wj9)py@@8"
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
+    "37965329762-o76niodu7ghvdl9ucj0ijhv22tlaq4ci.apps.googleusercontent.com"
+)
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
+EMAIL_HOST_PASSWORD = "osdm xsan tjlb lrpz"
+IMAGE_KIT_AUTH = {
+    "public_key": "public_tTc9vCi5O7L8WVAQquK6vQWNx08=",
+    "private_key": "private_edl1a45K3hzSaAhroLRPpspVRqM=",
+    "url_endpoint": "https://ik.imagekit.io/loayalsaid1/proLearningHub",
+}
